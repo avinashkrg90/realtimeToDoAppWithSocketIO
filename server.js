3bb809686872962ccf4be94946ffb8d1d23eae90
Avinash Kumar Gupta <robozealous@gmail.com>
2024-09-13 21:53:17 +0530

    initial setup for toDo app to work realtime with socket.io with multiple users

---
+++ server.js
@@ -0,0 +1,80 @@
+// Complete the server.js file to make user's add, delete and update the todos.
+import express from 'express';
+import http from 'http';
+import { Server } from 'socket.io';
+import cors from 'cors';
+import Task from './task.schema.js';
+
+export const app = express();
+app.use(cors());
+
+export const server = http.createServer(app);
+
+const io = new Server(server, {
+    cors: {
+        origin: '*',
+        methods: ["GET", "POST"]
+    }
+});
+
+io.on("connection", (socket) => {
+    console.log("Connection made.");
+
+    socket.on("join", async (data) => {
+      
+        // Broadcast a message to all other users in the same room
+        socket.broadcast.to(data.room).emit("message", {
+            text: `${data.username} has joined the room.`
+        });
+
+        try{
+            const toDos = await Task.find().sort({createdAt:1}).limit(50);
+            // console.log(toDos);
+            socket.emit('previousToDos', toDos)
+        }catch(err){
+            console.log(err)
+        }
+    });
+
+    socket.on("addToDo", async (data) => {
+
+        const toDo = new Task({
+            text: data.text,
+        })
+
+        await toDo.save();
+
+        // Broadcast the received toDo to all users in the same room
+        socket.broadcast.emit("newTodoAdded", data.text);
+    });
+
+    socket.on("deleteToDo", async (data) => {
+
+        const toDo = new Task({
+            text: data.text,
+        })
+
+        await toDo.save();
+
+        // Broadcast the received toDo to all users in the same room
+        socket.broadcast.emit("newTodoAdded", data.text);
+    });
+
+    socket.on("updateToDo", async (data) => {
+
+        const toDo = new Task({
+            text: data.text,
+        })
+
+        await toDo.save();
+
+        // Broadcast the received toDo to all users in the same room
+        socket.broadcast.emit("newTodoAdded", data.text);
+    });
+
+    socket.on("disconnect", () => {
+        console.log("Connection disconnected.");
+    });
+});
+
+
